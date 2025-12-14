1e62957c538e3d9189b33297ebcf185b59ddfdb0
abdessamademoussaif <abdessamademoussaif@gmail.com>
2025-12-14 20:28:01 +0100

    fix type in MyInfo

--- src/components/shared/MyInfo.tsx
+++ src/components/shared/MyInfo.tsx
@@ -1,4 +1,5 @@
-const MyInfo = ({ text, icon }) => (
+import { ReactNode } from 'react';
+const MyInfo = ({ text, icon }: { text: ReactNode; icon: ReactNode }) => (
   <div className="flex items-center gap-3 text-sm ">
     <span className="p-2 bg-muted rounded-full flex items-center justify-center">
       {icon}
